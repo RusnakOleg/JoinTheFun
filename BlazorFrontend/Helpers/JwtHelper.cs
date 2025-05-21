c0ba108382f05768f6090d15cc7307bffb14881a
Oleg <oleg.rusnak04@gmail.com>
2025-05-21 18:20:47 +0300

    added helers for auth

---
+++ BlazorFrontend/Helpers/JwtHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace BlazorFrontend.Helpers
+{
+    public static class UserContext
+    {
+        public static string UserId => "5ab3ca2e-2d7e-4856-8193-6c30df6c525b";
+    }
+}
+
---
+++ BlazorFrontend/Services/Auth/CustomAuthStateProvider.cs
@@ -0,0 +1,50 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorFrontend.Services.Auth
+{
+    public class CustomAuthStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _localStorage;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var token = await _localStorage.GetItemAsStringAsync("authToken");
+            var identity = string.IsNullOrWhiteSpace(token)
+                ? new ClaimsIdentity()
+                : new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+
+            var user = new ClaimsPrincipal(identity);
+            return new AuthenticationState(user);
+        }
+
+        public async Task NotifyUserAuthentication(string token)
+        {
+            await _localStorage.SetItemAsStringAsync("authToken", token);
+            var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+            var user = new ClaimsPrincipal(identity);
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+        }
+
+        public void NotifyUserLogout()
+        {
+            var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(anonymous)));
+        }
+
+        private IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.ReadJwtToken(jwt);
+            return token.Claims;
+        }
+    }
+}
