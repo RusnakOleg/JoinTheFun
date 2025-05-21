0a048293501be84fbb25605ed71aff7303c6afd5
Oleg <oleg.rusnak04@gmail.com>
2025-05-21 08:34:03 +0300

    added dependency inj for services

--- JoinTheFun.API/Program.cs
+++ JoinTheFun.API/Program.cs
@@ -6,6 +6,8 @@ using JoinTheFun.DAL.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using JoinTheFun.BLL.Mapping;
+using JoinTheFun.BLL.Services.Interfaces;
+using JoinTheFun.BLL.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,6 +19,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// ===== DB Conection =====
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -24,6 +27,7 @@ builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+// ===== DAL Repositories =====
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
 builder.Services.AddScoped<IInterestRepository, InterestRepository>();
@@ -35,7 +39,19 @@ builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IEventParticipantRepository, EventParticipantRepository>();
 builder.Services.AddScoped<IFollowRepository, FollowRepository>();
 
+// ===== AutoMapper =====
 builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// ===== BLL Services =====
+builder.Services.AddScoped<IEventService, EventService>();
+builder.Services.AddScoped<IEventParticipantService, EventParticipantService>();
+builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddScoped<IPostLikeService, PostLikeService>();
+builder.Services.AddScoped<IPostCommentService, PostCommentService>();
+builder.Services.AddScoped<IProfileService, ProfileService>();
+builder.Services.AddScoped<IInterestService, InterestService>();
+builder.Services.AddScoped<IUserInterestService, UserInterestService>();
+builder.Services.AddScoped<IFollowService, FollowService>();
 
 
 
