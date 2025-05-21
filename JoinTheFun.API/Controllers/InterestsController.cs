7e6f69a7d785af2f75b853ad10adc307b3862247
Oleg <oleg.rusnak04@gmail.com>
2025-05-21 10:15:57 +0300

    added InterestsController and UserInterestController

---
+++ JoinTheFun.API/Controllers/InterestsController.cs
@@ -0,0 +1,38 @@
+﻿using JoinTheFun.BLL.DTO.Interests;
+using JoinTheFun.BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoinTheFun.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class InterestsController : ControllerBase
+    {
+        private readonly IInterestService _interestService;
+
+        public InterestsController(IInterestService interestService)
+        {
+            _interestService = interestService;
+        }
+
+        /// <summary>
+        /// Отримати всі інтереси
+        /// </summary>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InterestDto>>> GetAll()
+        {
+            var interests = await _interestService.GetAllAsync();
+            return Ok(interests);
+        }
+
+        /// <summary>
+        /// Додати новий інтерес
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateInterestDto dto)
+        {
+            await _interestService.CreateAsync(dto);
+            return Ok(new { message = "Інтерес створено" });
+        }
+    }
+}
---
+++ JoinTheFun.API/Controllers/UserInterestController.cs
@@ -0,0 +1,49 @@
+﻿using JoinTheFun.BLL.DTO.Interests;
+using JoinTheFun.BLL.DTO.UserInterest;
+using JoinTheFun.BLL.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoinTheFun.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserInterestController : ControllerBase
+    {
+        private readonly IUserInterestService _userInterestService;
+
+        public UserInterestController(IUserInterestService userInterestService)
+        {
+            _userInterestService = userInterestService;
+        }
+
+        /// <summary>
+        /// Отримати всі інтереси профілю
+        /// </summary>
+        [HttpGet("{profileId}")]
+        public async Task<ActionResult<IEnumerable<InterestDto>>> GetByProfileId(int profileId)
+        {
+            var interests = await _userInterestService.GetByProfileIdAsync(profileId);
+            return Ok(interests);
+        }
+
+        /// <summary>
+        /// Додати інтерес до профілю
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] AddUserInterestDto dto)
+        {
+            await _userInterestService.AddAsync(dto);
+            return Ok(new { message = "Інтерес додано до профілю" });
+        }
+
+        /// <summary>
+        /// Видалити інтерес з профілю
+        /// </summary>
+        [HttpDelete]
+        public async Task<IActionResult> Remove([FromBody] AddUserInterestDto dto)
+        {
+            await _userInterestService.RemoveAsync(dto);
+            return Ok(new { message = "Інтерес видалено з профілю" });
+        }
+    }
+}
